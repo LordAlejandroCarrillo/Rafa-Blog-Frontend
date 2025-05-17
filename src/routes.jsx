f66f0ef5f9d51b2b69fa06a13c3a4f0f60737caf
LordAlejandroCarrillo <rafa.ccordon@gmail.com>
2025-05-16 23:41:43 -0600

    Arreglar errores

--- src/routes.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate } from 'react-router'
-import DashboardPage from './pages/DashboardPage.jsx'
-
-export const routes = [
-    {path: '/dashboard/publications', element: <DashboardPage/>},
-    {path: '/', element: <Navigate to={'/dashboard/publications/'}/>},
-]
